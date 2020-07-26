d61bda7803eb6e1e2043ad7001899ba26ae0a7f4
Karolina <karolinaa1999@gmail.com>
2020-07-26 14:57:52 +0100

    Add Stage 2/8 project

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,1 +1,8 @@
-print('H A N G M A N\nThe game will be available soon.')
+print('H A N G M A N')
+guess = input('Guess the word: ')
+correct_word = 'python'
+
+if guess == correct_word:
+    print('You survived!')
+else:
+    print('You are hanged!')
