efa7668ee5b562f16382a51ca9337362775a20a4
Karolina <karolinaa1999@gmail.com>
2020-07-27 18:28:07 +0100

    Add Stage 3/8 project
    The program randomly picks the word from a predefined list.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,8 +1,15 @@
-print('H A N G M A N')
-guess = input('Guess the word: ')
-correct_word = 'python'
+import random
 
-if guess == correct_word:
-    print('You survived!')
-else:
-    print('You are hanged!')
+
+def hangman():
+    words = ['python', 'java', 'kotlin', 'javascript']
+    random.seed()
+    correct_word = random.choice(words)
+
+    print('H A N G M A N')
+    guess = input('Guess the word: ')
+
+    print('You survived!' if guess == correct_word else 'You are hanged!')
+
+
+hangman()
