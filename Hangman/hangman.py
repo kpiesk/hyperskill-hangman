4d07eaf5df744184bf0fe4f5bc4c9e749391375a
Karolina <karolinaa1999@gmail.com>
2020-07-28 14:26:19 +0100

    Add Stage 4/8 project
    The program shows the first 3 letters of the randomly chosen word and hyphens instead of the other letters.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -5,11 +5,12 @@ def hangman():
     words = ['python', 'java', 'kotlin', 'javascript']
     random.seed()
     correct_word = random.choice(words)
+    help_word = correct_word[:3] + '-' * (len(correct_word) - 3)
 
     print('H A N G M A N')
-    guess = input('Guess the word: ')
-
-    print('You survived!' if guess == correct_word else 'You are hanged!')
+    user_guess = input(f'Guess the word: {help_word}')
+    print('You survived!' if user_guess == correct_word
+          else 'You are hanged!')
 
 
 hangman()
