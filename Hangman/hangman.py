7f84fda5cce5c71cf6e8ee54c54292b41b6ac266
Karolina <karolinaa1999@gmail.com>
2020-07-28 15:36:53 +0100

    Add Stage 5/8 project
    The program allows the user to guess the letters 8 times. If the guess is correct, that letter is uncovered.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,15 +2,33 @@ import random
 
 
 def hangman():
+    print('H A N G M A N')
     words = ['python', 'java', 'kotlin', 'javascript']
     random.seed()
     correct_word = random.choice(words)
-    help_word = correct_word[:3] + '-' * (len(correct_word) - 3)
+    guess_letters(correct_word)
 
-    print('H A N G M A N')
-    user_guess = input(f'Guess the word: {help_word}')
-    print('You survived!' if user_guess == correct_word
-          else 'You are hanged!')
+
+# allows the user to guess the letters 8 times
+def guess_letters(correct_word):
+    tries = 8
+    user_word = ['-'] * len(correct_word)
+
+    while tries > 0:
+        print(f"\n{''.join(user_word)}")
+        user_letter = input('Input a letter: ')
+
+        # if the user guesses the letter correctly, that letter is uncovered
+        if user_letter in correct_word:
+            for i in range(len(correct_word)):
+                if correct_word[i] == user_letter:
+                    user_word[i] = user_letter
+        else:
+            print('No such letter in the word')
+        tries -= 1
+
+    print("\nThanks for playing!"
+          "\nWe'll see how well you did in the next stage")
 
 
 hangman()
