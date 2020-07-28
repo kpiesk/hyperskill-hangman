16ecf83b9a4054f7c69dee75994b0c0806f93af1
Karolina <karolinaa1999@gmail.com>
2020-07-28 17:51:42 +0100

    Add Stage 6/8 project
    Now a player will have a lot of attempts and is limited only by the number of mistakes they make. A player can be mistaken 8 times and wins when all the letters are guessed and there are still some tries left.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -9,26 +9,39 @@ def hangman():
     guess_letters(correct_word)
 
 
-# allows the user to guess the letters 8 times
+# Allows the player to guess the word in 8 attempts
 def guess_letters(correct_word):
-    tries = 8
-    user_word = ['-'] * len(correct_word)
+    lives = 8  # lives are not lost if the guess is correct
+    player_word = ['-'] * len(correct_word)
 
-    while tries > 0:
-        print(f"\n{''.join(user_word)}")
-        user_letter = input('Input a letter: ')
+    while lives > 0:
+        print(f"\n{''.join(player_word)}")
+        # checks whether the user won the game
+        if '-' not in player_word:
+            print('You guessed the word!\nYou survived!')
+            break
+        player_letter = input('Input a letter: ')
 
-        # if the user guesses the letter correctly, that letter is uncovered
-        if user_letter in correct_word:
+        # checks whether the user has already attempted this letter
+        if player_letter in correct_word and player_letter in player_word:
+            print('No improvements')
+            # player loses a life if attempts the already guessed letter
+            lives -= 1
+
+        # checks whether the user guessed the letter correctly
+        elif player_letter in correct_word:
             for i in range(len(correct_word)):
-                if correct_word[i] == user_letter:
-                    user_word[i] = user_letter
+                # all cases of the guessed letter is uncovered
+                if correct_word[i] == player_letter:
+                    player_word[i] = player_letter
+
         else:
             print('No such letter in the word')
-        tries -= 1
+            # player loses a life if their guess is incorrect
+            lives -= 1
 
-    print("\nThanks for playing!"
-          "\nWe'll see how well you did in the next stage")
+    else:
+        print("You are hanged!")  # is only printed if the player lost
 
 
 hangman()
