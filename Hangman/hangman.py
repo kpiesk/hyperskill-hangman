69bec5de4dc386a2e0514c3d33022e73402fed82
Karolina <karolinaa1999@gmail.com>
2020-07-30 14:57:03 +0100

    Add Stage 8/8 project
    Now the program allows the player to replay the game multiple times, or exit the game.

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,26 +1,35 @@
 from random import choice
 from string import ascii_lowercase
+from typing import List
 
 
 # Allows the player to play the game Hangman
 def hangman():
-    print('H A N G M A N')
     words = ['python', 'java', 'kotlin', 'javascript']
-    word = choice(words)
-    guess_letters(word)
+    print('H A N G M A N')
+
+    while True:
+        game_choice = input('Type "play" to play the game, "exit" to quit: ')
+        if game_choice == 'exit':
+            break
+        elif game_choice == 'play':
+            word = choice(words)
+            guess_letters(word)
+        else:
+            continue
 
 
 # Allows the player to guess the letters of the randomly chosen word
 def guess_letters(word):
     lives = 8
-    hint = ['-'] * len(word)
+    hint: List[str] = ['-'] * len(word)
     guesses = set()
 
     while lives > 0:
         print(f"\n{''.join(hint)}")
         # checks whether the user won the game
         if '-' not in hint:
-            print('You guessed the word!\nYou survived!')
+            print('You guessed the word!\nYou survived!\n')
             break
         guess = input('Input a letter: ')
 
@@ -48,7 +57,7 @@ def guess_letters(word):
         guesses.add(guess)
 
     else:
-        print("You are hanged!")  # is only printed if the player lost
+        print('You are hanged!\n')  # is only printed if the player lost
 
 
 hangman()
